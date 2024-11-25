b0ef56e5482ec1fcd6471cb5ae765f258230e11a
mdnajishiqubal <notifynajish@gmail.com>
2024-11-25 21:58:20 +0530

    changes in experience

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,19 +11,19 @@ icon: "/skills/react.svg",
     projectType: ProjectType.Personal,
 githubUrl: "https://github.com/TechSocket-India-private-limited/techsocket",
 url: "https://techsocket.in/",
-    tags: ["Flutter", "Dart", "GetX", "Hive"],
+tags: ["nextjs", "mongodb", "React.js", "Material UI"],
   },
   {
 id: "Insure at Oasis",
 title: "OIF Tech",
     description:
-      "An e-commerce web application developed using React.js, Material UI, Redux, and Stripe.",
+"A variety of insurance-specific solutions for quoting and proposals, comparison, client and policy management, commissions management, compliance management.",
 icon: "/skills/php.svg",
     repoType: RepoType.Public,
     projectType: ProjectType.Personal,
 
 url: "https://oiftech.com/",
-    tags: ["React.js", "Redux", "Material UI", "Stripe"],
+tags: ["PHP", "Mysql", ],
   },
 
 ];
