644019811bb4327a5ec105db19cec39266babe68
mdnajishiqubal <notifynajish@gmail.com>
2024-11-25 21:51:35 +0530

    changes in TalkButton.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,9 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Nikhil Rajput",
+  title: "Najish Iqubal",
   description:
-    "Nikhil Rajput is a proficient Software Engineer and Full Stack Developer from India, skilled in front-end and back-end development using modern tech stacks.",
+      "Najish Iqubal is a proficient Software Engineer and Full Stack Developer from India, skilled in front-end and back-end development using modern tech stacks.",
   robots: {
     index: true,
     follow: true,
@@ -65,18 +65,7 @@ export const metadata: Metadata = {
       type: "image/x-icon",
     },
   ],
-  keywords: [
-    "nikhil rajput",
-    "nikhil",
-    "nixrajput",
-    "nikhil-rajput",
-    "rajput nikhil",
-    "founder of nixlab",
-    "nixlab founder",
-    "full stack developer",
-    "indian developer",
-    "nixrajput github",
-  ],
+
 };
 
 const GoogleAnalytics = dynamic(
