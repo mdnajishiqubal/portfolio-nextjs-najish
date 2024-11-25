97a73bbf20500695d535c48b64bd64531aee98de
mdnajishiqubal <notifynajish@gmail.com>
2024-11-25 21:06:26 +0530

    Button changes in Talk US

--- src/components/home/ui/TalkButton.tsx
+++ src/components/home/ui/TalkButton.tsx
@@ -1,15 +1,25 @@
 import Link from "next/link";
-import Strings from "@/constants/strings";
 
 const TalkButton = () => {
   return (
-    <Link
-      className="app__filled_btn min-w-[10rem]"
-      href={Strings.linkedInLink}
-      target="_blank"
-    >
-      Let&apos;s Talk
-    </Link>
+      <div className="flex space-x-4">
+          {/* Let's Talk Button (mailto) */}
+          <Link
+              className="app__filled_btn min-w-[10rem]"
+              href="mailto:notifynajish@gmail.com?subject=Let's%20Talk&body=Hi%20there,%0A%0AI%20would%20like%20to%20get%20in%20touch."
+          >
+              Let&apos;s Talk
+          </Link>
+
+          {/* Download CV Button */}
+          <Link
+              className="app__filled_btn min-w-[10rem]"
+              href="https://drive.google.com/file/d/1-gQ3Owt6NMYPSGdTZ4pxPVkupoYOP22g/view?usp=sharing"
+              target="_blank"
+          >
+              Download CV
+          </Link>
+      </div>
   );
 };
 
